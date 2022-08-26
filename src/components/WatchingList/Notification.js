02520a96f8156cb00c8c16d3bde24cf7a245ce8d
sergey <he305@mail.ru>
2022-08-26 14:03:52 +0300

    CON-64 convert time array to readable string

--- src/components/WatchingList/Notification.js
+++ src/components/WatchingList/Notification.js
@@ -5,11 +5,19 @@ const Notification = (props) => {
     const {notificationData} = props;
     console.log(notificationData)
 
+    const getReadableTime = (timeArray) => {
+        if (timeArray[1] / 10 < 1) {
+            timeArray[1] = '0' + timeArray[1];
+        } 
+        return timeArray[2] + '-' + timeArray[1] + '-' + timeArray[0] + ' ' +
+               timeArray[3] + ':' + timeArray[4] + ':' + timeArray[5];
+    }
+
     return (
         <div>
            {notificationData.map((item, index) => (
             <div key={index}>
-                <span>{item.time}</span>
+                <span>{getReadableTime(item.time)}</span>
                 <span>{item.data}</span>
             </div>
            ))}
