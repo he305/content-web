80b0f72bfe2be84d4fd0f50e2dbee2d47287bc30
Sergey <he305@mail.ru>
2022-08-26 14:06:16 +0300

    CON-64-web-fix-time (#17) #patch
    * CON-64 convert time array to readable string
    
    * CON-64 get rid of 0 adding

--- src/components/WatchingList/Notification.js
+++ src/components/WatchingList/Notification.js
@@ -5,11 +5,16 @@ const Notification = (props) => {
     const {notificationData} = props;
     console.log(notificationData)
 
+    const getReadableTime = (timeArray) => {
+        return timeArray[2] + '-' + timeArray[1] + '-' + timeArray[0] + ' ' +
+               timeArray[3] + ':' + timeArray[4] + ':' + timeArray[5];
+    }
+
     return (
         <div>
            {notificationData.map((item, index) => (
             <div key={index}>
-                <span>{item.time}</span>
+                <span>{getReadableTime(item.time)}</span>
                 <span>{item.data}</span>
             </div>
            ))}
